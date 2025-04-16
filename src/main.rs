2ef9f9b7c4ee573a5c3bcc4b7183bd04b5f6cf61
Murali raju <17339891+muralishub@users.noreply.github.com>
2025-04-16 16:55:28 +0100

    response in separate function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 #![allow(unused)]
 
 
-use axum::{response::Html, serve, Router};
+use axum::{response::{Html, IntoResponse}, serve, Router};
 use tokio::net::TcpListener;
 
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-                                 .route("/", axum::routing::get(|| async {Html("hello <Strong>world</Strong>")}));
+                                 .route("/", axum::routing::get(|| hello_handler()));
   
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
    
     serve(listener, app).await.unwrap();
 }
+
+async fn hello_handler() -> impl IntoResponse {
+
+    println!("->> {:12} - hello_handler", "Handler");
+    Html("Hello <Strong>world!</Strong>")
+
+
+
+    }
