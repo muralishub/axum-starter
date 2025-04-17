4126c4db73eb37f4451bf753fa17652fa440e132
Murali raju <17339891+muralishub@users.noreply.github.com>
2025-04-17 15:28:02 +0100

    query string sample

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 #![allow(unused)]
 
 
-use axum::{response::{Html, IntoResponse}, serve, Router};
+use axum::{extract::Query, response::{Html, IntoResponse}, serve, Router};
 use tokio::net::TcpListener;
+use serde::Deserialize;
 
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-                                 .route("/", axum::routing::get(|| hello_handler()));
+                                 .route("/hello", axum::routing::get(hello_handler));
   
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
    
     serve(listener, app).await.unwrap();
 }
+#[derive(Debug, Deserialize)]
+struct HelloParams{
+    name: Option<String>,
+}
 
-async fn hello_handler() -> impl IntoResponse {
 
-    println!("->> {:12} - hello_handler", "Handler");
-    Html("Hello <Strong>world!</Strong>")
+
+async fn hello_handler(Query(params): Query<HelloParams>) -> impl IntoResponse {
+
+    println!("->> {:12} - hello_handler- {params:?}", "Handler");
+
+    let name = params.name.as_deref().unwrap_or("world");
+
+    Html(format!("Hello <Strong{name}</Strong>"))
 
 
 
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -7,7 +7,7 @@ async fn quick_dev() -> Result<()> {
 
 let hc = httpc_test::new_client("http://localhost:3000")?;
 
-hc.do_get("").await?.print().await?;
+hc.do_get("/hello?name=mike").await?.print().await?;
 
 Ok(())
 
