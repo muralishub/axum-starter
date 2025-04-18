737d561a213197d2191cbecc3a3c93c3fd099e5d
Murali raju <17339891+muralishub@users.noreply.github.com>
2025-04-18 07:53:53 +0100

    route with path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused)]
 
 
-use axum::{extract::Query, response::{Html, IntoResponse}, serve, Router};
+use axum::{extract::Query, http::header::InvalidHeaderName, response::{Html, IntoResponse}, serve, Router};
+use axum::extract::path::Path;
 use tokio::net::TcpListener;
 use serde::Deserialize;
 
@@ -9,7 +10,10 @@ use serde::Deserialize;
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-                                 .route("/hello", axum::routing::get(hello_handler));
+                                 .route("/hello", axum::routing::get(hello_handler))
+                               // .route("/hello2/:name", axum::routing::get(hello_handler2) this is old way of doing things 0.6 verison of axum
+                               .route("/hello2/{name}", axum::routing::get(hello_handler2)
+                                );
   
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
    
@@ -23,13 +27,14 @@ struct HelloParams{
 
 
 async fn hello_handler(Query(params): Query<HelloParams>) -> impl IntoResponse {
-
-    println!("->> {:12} - hello_handler- {params:?}", "Handler");
-
+   println!("->> {:12} - hello_handler- {params:?}", "Handler");
     let name = params.name.as_deref().unwrap_or("world");
-
-    Html(format!("Hello <Strong{name}</Strong>"))
-
-
-
+    Html(format!("Hello <Strong>{name}</Strong>"))
     }
+
+    async fn hello_handler2(Path(name): Path<String>) -> impl IntoResponse {
+        println!("->> {:12} - hello_handler- {name:?}", "Handler");
+         
+         Html(format!("Hello <Strong>{name}</Strong>"))
+         }
+
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -7,9 +7,20 @@ async fn quick_dev() -> Result<()> {
 
 let hc = httpc_test::new_client("http://localhost:3000")?;
 
-hc.do_get("/hello?name=mike").await?.print().await?;
+hc.do_get("/hello?name=matt").await?.print().await?;
 
 Ok(())
 
 
 }
+#[tokio::test]
+async fn quick_dev2() -> Result<()> {
+
+    let hc = httpc_test::new_client("http://localhost:3000")?;
+    
+    hc.do_get("/hello2/mike2").await?.print().await?;
+    
+    Ok(())
+    
+    
+    }
